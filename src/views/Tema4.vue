6b12b23a66b468788fe640d49cd6cd5c369b0dee
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-04-27 20:17:16 -0500

    V0.5

---
+++ src/views/Tema4.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 4
+      .h3 Depuración y fallas de sintaxis
+    
+</template>
+
+<script>
+export default {
+  name: 'Tema4',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/Tema5.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 5
+      .h3 Fallas de lógica  
+    
+</template>
+
+<script>
+export default {
+  name: 'Tema5',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
